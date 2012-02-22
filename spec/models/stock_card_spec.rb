2ecf050c364e052b708fddb8512fbdcbc7f13cc9
Leo Tumwattana <leo.tumwattana@gmail.com>
2012-02-22 12:23:16 +0800

    Added more stock_card examples

--- spec/models/stock_card_spec.rb
+++ spec/models/stock_card_spec.rb
@@ -20,6 +20,7 @@ describe StockCard do
       stock_card.should have_at_least(1).error_on(:sku)
     end
   end
+  
   describe "name" do
     it "should not be blank" do
       should_not_have_blank(:name)
@@ -31,7 +32,22 @@ describe StockCard do
       stock_card.should be_valid
     end
   end
+  
   describe "notes" do
     it "should not have more than x characters"
+  end
+  
+  describe "should have" do
+    it "many offers" do
+      StockCard.new.should respond_to(:offers)
+    end
+
+    it "many assignments" do
+      StockCard.new.should respond_to(:assignments)
+    end
+
+    it "many sections" do
+      StockCard.new.should respond_to(:sections)
+    end
   end
 end
